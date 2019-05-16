209e59253b93e50a2a762fc5f214aadd485b6ca9
Na27i <oanazzi.setter@gmail.com>
2019-05-16 15:14:25 +0900

    add program to post new tweet.

---
+++ post_tweet.py
@@ -0,0 +1,32 @@
+import json
+import settings
+
+from requests_oauthlib import OAuth1Session
+
+CK = settings.CONSUMER_KEY
+CS = settings.CONSUMER_SECRET
+AT = settings.ACCESS_TOKEN
+ATS = settings.ACCESS_TOKEN_SECRET
+twitter = OAuth1Session(CK, CS, AT, ATS)
+
+url = "https://api.twitter.com/1.1/statuses/update.json"
+
+tweet = ''
+
+print("Please input new tweet.")
+while 1:
+    sent = input('>> ')
+    if sent == "quit":
+        break
+    else:
+        tweet += sent
+        tweet += '\n'
+
+params = {"status" : tweet}
+
+res = twitter.post(url, params = params)
+
+if res.status_code == 200:
+    print("投稿成功")
+else:
+    print("投稿失敗")
